4d45fbbccd66ea2e049b5d32c010143e1554b054
MaxiBelli <maximiliano_belli@hotmail.com>
2023-06-19 17:25:54 -0300

    feat: Implement NavBar and SideBar components

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,16 +1,18 @@
+import { Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
-      {/* <NavBar drawerWidth={drawerWidth} />
+      <NavBar drawerWidth={drawerWidth} />
 
-      <SideBar drawerWidth={drawerWidth} /> */}
+      <SideBar drawerWidth={drawerWidth} />
 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/* <Toolbar /> */}
+        <Toolbar />
 
         {children}
       </Box>
