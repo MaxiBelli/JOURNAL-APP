e9cdd84d39925d1e24dd7f3893c2bf5c1610bf65
MaxiBelli <maximiliano_belli@hotmail.com>
2023-06-19 17:19:35 -0300

    feat: Implement JournalLayout and JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,17 @@
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
-  )
-}
+    <JournalLayout>
+      <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography>
+
+      {/* <NothingSelectedView />
+      <NoteView /> */}
+
+    
+    </JournalLayout>
+  );
+};
