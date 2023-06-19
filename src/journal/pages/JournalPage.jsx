b270b6b57a1a191a7522c8f1c9e5cea2b7cc46e1
MaxiBelli <maximiliano_belli@hotmail.com>
2023-06-19 17:45:50 -0300

    feat: Implement NothingSelectedView-NoteView  and ImageList component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,17 +1,14 @@
-import { IconButton } from "@mui/material";
-import { AddOutlined } from "@mui/icons-material";
+
 
 import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography>
+      {/* <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography> */}
 
-      {/* <NothingSelectedView />
-      <NoteView /> */}
-
-    
+      <NothingSelectedView />
+      {/* <NoteView /> */}
     </JournalLayout>
   );
 };
