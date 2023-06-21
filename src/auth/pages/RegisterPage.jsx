bd7fa1333ec081d3403754cd68d2832b60ee99ab
MaxiBelli <maximiliano_belli@hotmail.com>
2023-06-21 17:31:16 -0300

    feat: Create user with email and password

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link as RouterLink } from "react-router-dom";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "../layout/AuthLayout";
 import { useForm } from "../../hooks";
+import { startCreatingUserWithEmailPassword } from "../../store/auth";
 
 const formData = {
   email: "",
@@ -20,6 +22,8 @@ const formValidations = {
 };
 
 export const RegisterPage = () => {
+
+  const dispatch = useDispatch
   const [formSubmitted, setFormSubmitted] = useState(false);
 
   const {
@@ -40,7 +44,10 @@ export const RegisterPage = () => {
   const onSubmit = (event) => {
     event.preventDefault();
     setFormSubmitted(true);
-    console.log(formState);
+  
+    if(!isFormValid)return;
+
+    dispatch(startCreatingUserWithEmailPassword(formState))
   };
 
   return (
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { singInWithGoogle } from "../../firebase/providers";
+import { singInWithGoogle , registerUserWithEmailPassword } from "../../firebase/providers";
 import { checkingCredentials, logout, login } from "./";
 
 export const checkingAuthentication = () => {
@@ -12,6 +12,25 @@ export const startGoogleSignIn = () => {
     dispatch(checkingCredentials());
 
     const result = await singInWithGoogle();
+    if (!result.ok) return dispatch(logout(result.errorMessage));
+
+    dispatch(login(result));
+  };
+};
+
+export const startCreatingUserWithEmailPassword = ({
+  email,
+  password,
+  displayName,
+}) => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+
+    const result = await registerUserWithEmailPassword({
+      email,
+      password,
+      displayName,
+    });
     if (!result.ok) return dispatch(logout(result.errorMessage));
 
     dispatch(login(result));
