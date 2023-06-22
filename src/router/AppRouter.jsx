32f9392d162d99cdf97f8b58b5117edf2a502a67
MaxiBelli <maximiliano_belli@hotmail.com>
2023-06-22 10:19:20 -0300

    feat: Checking authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
-
-import { AuthRoutes } from '../auth/routes/AuthRoutes';
-import { JournalRoutes } from '../journal/routes/JournalRoutes';
-
+import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
+
   return (
     <Routes>
+      {/* Login and Register */}
+      <Route path="/auth/*" element={<AuthRoutes />} />
 
-        {/* Login and Register */}
-        <Route path="/auth/*" element={ <AuthRoutes /> } />
-
-        {/* JournalApp */}
-        <Route path="/*" element={ <JournalRoutes /> } />
-
+      {/* JournalApp */}
+      <Route path="/*" element={<JournalRoutes />} />
     </Routes>
-  )
-}
+  );
+};
