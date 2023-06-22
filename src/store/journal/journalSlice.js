990e3818eb926da7ebf33f1ddbb336c805667c27
MaxiBelli <maximiliano_belli@hotmail.com>
2023-06-22 16:53:36 -0300

    feat: Add JournalSlice for managing journal entries

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const journalSlice = createSlice({
+  name: "journal",
+  initialState: {
+    isSaving: false,
+    messageSaved: "",
+    notes: [],
+    active: null,
+    // active: {
+    //     id: 'ABC123',
+    //     title: '',
+    //     body: '',
+    //     date: 1234567,
+    //     imageUrls: [], // https://foto1.jpg, https://foto2.jpg, https://foto3.jpg
+    // }
+  },
+  reducers: {
+    savingNewNote: (state) => {
+      state.isSaving = true;
+    },
+    addNewEmptyNote: (state, action) => {},
+    setActiveNote: (state, action) => {},
+    setNotes: (state, action) => {},
+    setSaving: (state) => {},
+    updateNote: (state, action) => {
+      // payload: note
+    },
+
+    deleteNoteById: (state, action) => {},
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const {
+  addNewEmptyNote,
+  deleteNoteById,
+  savingNewNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+} = journalSlice.actions;
