1d877b2e3b57036d015475b6d732bfd42b0127b9
MaxiBelli <maximiliano_belli@hotmail.com>
2023-06-23 10:49:52 -0300

    feat: Load notes from Firestore

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,12 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, setActiveNote } from "./journalSlice";
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+  setNotes,
+} from "./journalSlice";
+import { loadNotes } from "../../helpers";
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
@@ -21,5 +27,15 @@ export const startNewNote = () => {
 
     dispatch(addNewEmptyNote(newNote));
     dispatch(setActiveNote(newNote));
+  };
+};
+
+export const startLoadingNotes = () => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
+    if (!uid) throw new Error("User UID does not exist");
+
+    const notes = await loadNotes(uid);
+    dispatch(setNotes(notes));
   };
 };
